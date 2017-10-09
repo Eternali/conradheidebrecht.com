7fb4c3bfa09d579a3a0606db47ad409d2494021d
Conrad Heidebrecht <conrad.heidebrecht@gmail.com>
2017-10-09 15:38:47 -0400

    added custom background

--- js/vertex.js
+++ js/vertex.js
@@ -2,27 +2,11 @@ function Vertex () {
 
     this.pos = { x: random(vertexRad, width-vertexRad), y: random(vertexRad, height-vertexRad) };
     this.vel = { x: random(-1.5, 1.5), y: random(-1.5, 1.5) };
-    this.acc = { x: 0, y: 0 };
-    this.dmouse = 0;
-    this.dmousereg = 0;
 
     this.update = function () {
 
-        this.dmouse = sqrt(pow(this.pos.x-winMouseX, 2) + pow(this.pos.y-winMouseY, 2));
-        this.dmousereg = map(this.dmouse, 0, sqrt(pow(width, 2) + pow(height, 2)), 0, 1);
-
-        if (this.dmouse <= mouseDist) {
-            this.acc.x = mouseVec.x * (1-this.dmousereg) * vertexMass;
-            this.acc.y = mouseVec.y * (1-this.dmousereg) * vertexMass;
-        } else {
-            this.acc.x = 0;
-            this.acc.y = 0;
-            if (abs(this.vel.x) > maxRestSpeed) this.vel.x *= friction;
-            if (abs(this.vel.y) > maxRestSpeed) this.vel.y *= friction;
-        }
-
-        this.vel.x += this.acc.x;
-        this.vel.y += this.acc.y;
+        this.vel.x += random(-0.01, 0.01);
+        this.vel.y += random(-0.01, 0.00);
 
         if (this.pos.x < vertexRad) this.vel.x = abs(this.vel.x);
         else if (width - vertexRad < this.pos.x) this.vel.x = -abs(this.vel.x);
