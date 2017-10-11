0d915d5432084e97fb184617eefe77d47aec1e92
conrad <conrad.heidebrecht@gmail.com>
2017-10-11 15:10:20 -0400

    title animation finally correct

--- conradheidebrecht/public/javascripts/textanimation.js
+++ conradheidebrecht/public/javascripts/textanimation.js
@@ -1,5 +1,43 @@
 jQuery.fn.letterDrop = function () {
+    let obj = this;
 
+    let drop = {
+        arr: obj.text().split(''),
+
+        range: {
+            min: 1,
+            max: 9
+        },
+
+        // generate extra CSS
+        styles: function () {
+            let dropDelays = '\n', addCSS;
+            for (i = this.range.min; i <= this.range.max; i++ )
+                dropDelays += '.ld' + i + ' { animation-delay: 1.' + i + 's; }\n';
+
+            addCSS = $('<style>' + dropDelays + '</style>');
+            $('head').append(addCSS);
+        },
+
+        main: function () {
+            let dp = 0;
+            obj.text('');
+            $.each(this.arr, function (index, value) {
+                dp = dp.randomInt(drop.range.min, drop.range.max);
+                if (value === ' ') value = '&nbsp';
+                obj.append('<span class="letterDrop ld' + dp + '">' + value + '</span>');
+            });
+        }
+    };
+
+    // make it easier to generate random integers within a range
+    Number.prototype.randomInt = function (min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    };
+
+    // create styles and initiate them
+    drop.styles();
+    drop.main();
 };
 
 $(document).ready(function () {
@@ -9,5 +47,8 @@ $(document).ready(function () {
         $('#nav').find('li').removeClass('current');
         $(this).addClass('current');
     });
+
+    // animate name drop
+    $('.title-name').letterDrop();
 
 });
