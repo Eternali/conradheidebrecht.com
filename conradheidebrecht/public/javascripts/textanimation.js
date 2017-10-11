6c4fa55fe82d07e74929d7869e135a430f412dda
conrad <conrad.heidebrecht@gmail.com>
2017-10-10 22:37:38 -0400

    working on title orientation

---
+++ conradheidebrecht/public/javascripts/textanimation.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+
+    // change the color of the navbar elements
+    $('#nav').on('click', 'li', function () {
+        $('#nav').find('li').removeClass('current');
+        $(this).addClass('current');
+    });
+
+});
