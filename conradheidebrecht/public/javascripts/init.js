9663f195fe2aebc0f202336a3f67eca27a55d116
conrad <conrad.heidebrecht@gmail.com>
2017-10-11 18:14:32 -0400

    working on about section and scrolling functionality

---
+++ conradheidebrecht/public/javascripts/init.js
@@ -0,0 +1,9 @@
+$('.smoothscroll').on('click', function (e) {
+    // stop page from moving to requested section
+    e.preventDefault();
+
+    let target = this.hash,
+    $target = $(target);
+
+    
+});
