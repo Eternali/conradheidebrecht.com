2838a6a59bb07787f496da07436ae549e2e87a1b
conrad <conrad.heidebrecht@gmail.com>
2017-10-21 16:09:56 -0400

    got ajax with node and express working

--- conradheidebrecht/public/javascripts/sendemail.js
+++ conradheidebrecht/public/javascripts/sendemail.js
@@ -15,46 +15,16 @@
 
 
 function sendEmail () {
-    let req = new XMLHttpRequest();
-    req.onreadystatechange = function () {
-        if (req.readyState === 4) {
-            if (req.status !== 200) {
-                document.getElementById('emailsuccess').innerHTML = "errors.";
-            }
-            else {
-                let response = JSON.parse(req.responseText);
-                document.getElementById('emailsuccess').innerHTML = response.success;
-            }
-        }
+
+    let parameters = {
+        selfemail: 'crbheide@edu.uwaterloo.ca',
+        name: $('#contactName').val(),
+        email: $('#contactEmail').val(),
+        subject: $('#contactSubject').val(),
+        message: $('#contactMessage').val()
     };
-    req.open('POST', '/sendemail');
-    req.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    $.get('/sendemail', parameters, function (data) {
+        $('#emailresult').html(data);
+    });
 
-    let selfemail = 'crbheide@edu.uwaterloo.ca';
-    let name = document.getElementById('contactName').value;
-    let email = document.getElementById('contactEmail').value;
-    let subject = document.getElementById('contactSubject').value;
-    let message = document.getElementById('contactMessage').value;
-
-    // encode the post request manually, without jquery
-    req.send(encodePostVars({
-                            'selfemail': selfemail,
-                            'name': name,
-                            'email': email,
-                            'subj': subject,
-                            'message': message
-                        }));
-
-    return false;
-}
-
-function encodePostVars (valsDict) {
-    let encodedStr = '';
-    let valkeys = Object.keys(valsDict);
-    for (let val in valsDict) {
-        if (valkeys[valkeys.length-1] == val) encodedStr += val + '=' + valsDict[val];
-        else encodedStr += val + '=' + valsDict[val] + '&';
-    }
-
-    return encodedStr;
 }
--- conradheidebrecht/routes/index.js
+++ conradheidebrecht/routes/index.js
@@ -9,13 +9,19 @@ router.get('/', function(req, res, next) {
     res.render('index', { title: 'Profile - Conrad Heidebrecht' });
 });
 /* POST requests for sending email. */
-router.post('/sendemail', function (req, res, next) {
+router.get('/sendemail', function (req, res) {
+    let self = req.query.selfemail;
+    let name = req.query.name;
+    let email = req.query.email;
+    let subj = req.query.subject;
+    let message = req.query.message;
+
     // use sendgrid api to send messages
     const msg = {
-        to: email,
-        from: selfemail,
+        to: self,
+        from: email,
         subject: subj,
-        text: message
+        text: '<' + name + '> wrote: \n' + message
     };
     sgMail.send(msg);
 });
