c22df74b91c0070e5e8121ab56671b493c390ac1
Conrad Heidebrecht <conrad.heidebrecht@gmail.com>
2018-05-25 20:22:38 -0400

    initial vue commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script>
+import logo from './assets/logo.png'
+import ProjectCard from './components/ProjectCard.vue'
+
+export default {
+  name: 'app',
+  components: {
+    ProjectCard
+  },
+  render() {
+    return (
+      <div id="app">
+        <ProjectCard msg='Welcome to JSX' img={ logo } />
+      </div>
+    );
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+#app {
+  font-family: 'Avenir', 'Helvetica', 'Arial', 'sans-serif';
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,39 @@
+<script>
+export default {
+  name: 'ProjectCard',
+  components: {
+    VModel: Buefy.VModel
+  },
+  props: {
+    title: String,
+    msg: String,
+    footer: String,
+    img: String,
+    link: String
+  },
+  render() {
+    return (
+      <b-model width='640' scroll='keep'>
+        <div class='card'>
+          <div class='card-image'>
+            <figure class='image is-4by3'>
+              <img src={ this.img } alt='Image' />
+            </figure>
+          </div>
+          <div class='card-content'>
+            <div class='media'>
+              <div class='media-content'>
+                <p class='title is-4'>{ this.title }</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </b-model>
+    );
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+
+</style>
