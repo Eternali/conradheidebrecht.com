2c70ed2a9c47d6003b4fb70d64274fb85410aff8
Conrad Heidebrecht <conrad.heidebrecht@gmail.com>
2018-06-15 16:01:36 -0400

    sync

--- src/components/MainAttraction.vue
+++ src/components/MainAttraction.vue
@@ -16,30 +16,32 @@ export default {
   },
   render() {
     return (
-      <v-container flex fill-height>
-        <v-layout align-center>
-          <v-flex xs12>
-            <h3 class='display-3 titlename'>
-              <span style={ `font-weight: 100; color: ${this.$vuetify.theme.secondary}` }>
-                { this.firstname }
-              </span> &nbsp;
-              <span style={ `font-weight: 700; color: ${this.$vuetify.theme.primary}` }>
-                { this.lastname }
-              </span>
-            </h3>
-            < br/>
-            <v-flex xs8 offset-xs2>
-              <h4 class='subtitle center'>{ this.subtitle }</h4>
-              <v-divider class='my-3'></v-divider>
+      <v-content fill-height>
+        <v-container flex>
+          <v-layout align-center>
+            <v-flex xs12>
+              <h3 class='display-3 titlename'>
+                <span style={ `font-weight: 100; color: ${this.$vuetify.theme.secondary}` }>
+                  { this.firstname }
+                </span> &nbsp;
+                <span style={ `font-weight: 700; color: ${this.$vuetify.theme.primary}` }>
+                  { this.lastname }
+                </span>
+              </h3>
+              < br/>
+              <v-flex xs8 offset-xs2>
+                <h4 class='subtitle center'>{ this.subtitle }</h4>
+                <v-divider class='my-3'></v-divider>
+              </v-flex>
+              <ul class='socialList'>
+                { this.links.map((link) =>
+                  <v-btn color='primary' fab flat large href={ link.href }>{ link.child }</v-btn>
+                ) }
+              </ul>
             </v-flex>
-            <ul class='socialList'>
-              { this.links.map((link) =>
-                <v-btn color='primary' fab flat large href={ link.href }>{ link.child }</v-btn>
-              ) }
-            </ul>
-          </v-flex>
-        </v-layout>
-      </v-container>
+          </v-layout>
+        </v-container>
+      </v-content>
     );
   }
 }
