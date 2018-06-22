ec865e366c8cd00a00bbf1253e1705551764cdd4
Conrad Heidebrecht <conrad.heidebrecht@gmail.com>
2018-06-21 23:18:11 -0400

    almost done

--- src/components/MainAttraction.vue
+++ src/components/MainAttraction.vue
@@ -6,6 +6,12 @@ export default {
     subtitle: Object,
     links: Array
   },
+  data() {
+    return {
+      speed: 100,
+      pause: 200
+    }
+  },
   computed: {
     firstname() {
       return this.name.split(' ')[0].toUpperCase();
@@ -14,12 +20,33 @@ export default {
       return this.name.split(' ')[1].toUpperCase();
     }
   },
+  methods: {
+    typeWriter(i, offset) {
+      if (i < this.name.length) {
+        if (i < this.firstname.length) {
+          this.$refs.firstname.innerHTML += this.firstname.charAt(i);
+        } else {
+          if (i === this.firstname.length) offset = this.firstname.length + 1;
+          if (i - offset < this.lastname.length)
+            this.$refs.lastname.innerHTML += this.lastname.charAt(i - offset);
+        }
+        i++;
+        setTimeout(() => this.typeWriter(i, offset), this.speed);
+      }
+    }
+  },
+  mounted() {
+    this.typeWriter(0, 0);
+  },
   render() {
     return (
       <v-container app fluid style='height: 100vh' id='home'>
         <v-layout fill-height align-center>
           <v-flex xs12>
-            <h3 class='display-3 titlename'>{ this.name.toUpperCase() }</h3>
+            <h3 class='display-3'>
+              <span class='letterdrop' ref='firstname'></span>
+              <span class='letterdrop' ref='lastname'></span>
+            </h3>
             < br/>
             <v-flex xs8 offset-xs2>
               <h4 class='subtitle center'>{ this.subtitle }</h4>
@@ -46,17 +73,6 @@ export default {
   justify-content center
   align-items center
 
-.titlename
-  font-family TimeBurner
-  letter-spacing 0.3em !important
-  word-wrap break-word
-  word-spacing -0.5em
-  font-weight 700
-  color $primary
-  &:nth-child(-n+6)
-    font-weight 100
-    color $secondary
-
 .subtitle
   font-size 1.4em
   font-weight normal
@@ -66,25 +82,29 @@ export default {
   list-style-type none
   padding-right 20px
 
-.letterDrop
-  position relative
-  top -4.75em
-  display inline-block
-  text-transform uppercase
-  opacity .8
-  transform rotateX(-90deg)
-  animation letterDrop 1.2s ease 1 normal forwards
+h3 > span
+  font-family TimeBurner
+  letter-spacing 0.3em !important
+  word-wrap break-word
+  word-spacing -0.5em
+  &:nth-child(1)
+    color $secondary
+    font-weight 700
+  &:nth-child(1):after
+    content ''
+    width 0.5em
+    height 2em
+    background grey
+  &:nth-child(2)
+    color $primary
+    font-weight 100
 
-@keyframes letterDrop
-  10%
-    opacity .5
-  20%
-    opacity .8
-    top -1.75em
-    transform rotateX(-360deg)
+@keyframes cursor
+  0%
+    opacity 0.0
+  50%
+    opacity 1.0
   100%
-    opacity 1
-    top -.75em
-    transform rotateX(360deg)
+    opacity 0.0
 
 </style>
